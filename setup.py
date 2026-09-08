90e7489e47a1163f2076b59c307a34aa2eebdba5
Connor Frankston <con.frankst@gmail.com>
2026-09-08 10:48:44 -0700

    Fix native wheel packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+
+
+class BinaryDistribution(Distribution):
+    """Distribution containing platform-specific package data."""
+
+    def has_ext_modules(self):
+        return True
+
 
 setup(
     name="data_graph",
     version="1.0.0",
     packages=find_packages(),
+    package_data={
+        'data_graph': ['*.so', '*.cpp'],
+    },
+    distclass=BinaryDistribution,
     install_requires=[
+        'scikit-network',
         "numpy",
         "scipy",
         "scikit-learn",
